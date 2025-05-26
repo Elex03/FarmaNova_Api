afc9864f7c4a4f1a151dc5cc6bf4d33ffaec4852
Eliezer <eacuna042@gmail.com>
2025-05-25 21:32:19 -0600

    add: newDB

--- src/controllers/medicals.ts
+++ src/controllers/medicals.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express";
+import { Prismaclient } from "../constants/db";
+import { EstadoMedicamento, EstadoMedicamentoExpirado } from "@prisma/client";
 
 export const createMedicine = async (req: Request, res: Response) => {
-  const { nombre, accionTeraId, fabricante, dosis, via } = req.body;
+  const { nombre, via, accionTerapeuticaIds, formaFarmaceutica} =
+    req.body;
 
-2
+  const result = await Prismaclient.$transaction(async (tx) => {
+    const medicine = await tx.medicamento.create({
+      data: {
+        descripcion: nombre,
+        imagen: 'uploads/'+ req.file?.filename || null,
+        via,
+        EstadoMedicamento: EstadoMedicamento.DISPONIBLE, 
+        EstadoMedicamentoExpirado: EstadoMedicamentoExpirado.DISPONIBLE,
+        forma_fk: Number(formaFarmaceutica),
+      },
+    });
 
-  console.log(req.file, req.body)
-  res.send({ nombre, accionTeraId, fabricante, dosis, via , msg:"Jelo"});
+    await tx.accionmedicamentos.createMany({
+      data: accionTerapeuticaIds.map((accionId: number) => ({
+        medicamento_fk: medicine.medicamento_pk,
+        accionTerapeutica_fk: Number(accionId),
+      })),
+    });
 
+    return medicine;
+  });
 
+  res.send(result);
 };
